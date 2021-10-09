681aad0940d9f0a5abedf82e805eccd2b08848c0
Alexander Biezbardis <mr.biezbardis@gmail.com>
2021-10-09 23:14:54 +0300

    Revision 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,24 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        final ExecutorService executorService = Executors.newFixedThreadPool(4);
+        final Dealer dealer = Dealer.getInstance();
+
+        Thread thread1 = new Thread(null, dealer::sellCar, "Вася");
+        Thread thread2 = new Thread(null, dealer::sellCar, "Петя");
+        Thread thread3 = new Thread(null, dealer::sellCar, "Лена");
+        Thread thread4 = new Thread(null, dealer::deliverCar, "Дилер");
+
+        executorService.submit(thread1);
+        executorService.submit(thread2);
+        executorService.submit(thread3);
+        executorService.submit(thread4);
+
+        executorService.shutdown();
     }
 }
